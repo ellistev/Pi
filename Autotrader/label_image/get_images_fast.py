9a0af91ab324757299a4ae56ece9ca2c0fa0461a
Steven Elliott <steven.elliott@trader.ca>
2017-02-20 15:26:58 -0800

    stuff

--- Autotrader/label_image/get_images_fast.py
+++ Autotrader/label_image/get_images_fast.py
@@ -6,12 +6,12 @@ filename = ''
 lines = tuple(open('corolla.txt', 'r'))
 photohelperurl = 'http://az413908.vo.msecnd.net'
 underscore = '_'
+
 def getimg(count):
-    localpath = '{0}/images/{0}{1}.jpg'.format(carname, underscore+str(count))
-    urllib.request.urlretrieve(photohelperurl + lines[count], localpath)
+    localpath = '{0}/{0}{1}.jpg'.format(carname, underscore+str(count))
+    urllib.request.urlretrieve(photohelperurl + "/" + lines[count], localpath)
     #URLS[count] = localpath
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=50) as e:
-    for i in range(1):
-        getimg(1)
-        #e.submit(getimg, i)
+    for i in range(1000):
+        e.submit(getimg, i)

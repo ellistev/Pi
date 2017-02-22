4f47c2ba9598eb5eb8d9badb951f7c2fe964541f
root <root@devtdrStevenElliotHack.ojsczv4b4l4ezcky4z5wx5wasc.bx.internal.cloudapp.net>
2017-02-22 18:01:45 +0000

    not sure

--- Autotrader/rekognizeBoto.py
+++ Autotrader/rekognizeBoto.py
@@ -7,7 +7,7 @@ import json
 
 client = boto3.client('rekognition')
 
-with open('/datadrive/prepared_photos_lite/ford_fiesta/ford_fiesta_4.jpg', 'rb') as source_image:
+with open('/datadrive/prepared_photos_lite/ford_fiesta/ford_fiesta_5031.jpg', 'rb') as source_image:
     source_bytes = source_image.read()
 
 response = client.detect_labels(

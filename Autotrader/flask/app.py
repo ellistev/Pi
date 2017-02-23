3f792276042530a074f7f948e3610d78702396e4
Steven Elliott <steven.elliott@trader.ca>
2017-02-22 17:57:05 -0800

    fawfw

--- Autotrader/flask/app.py
+++ Autotrader/flask/app.py
@@ -59,7 +59,7 @@ def run_inference_on_image(image_data):
             human_string = labels[node_id]
             score = predictions[node_id]
             print('%s (score = %.5f)' % (human_string, score))
-            resultData[human_string] = str(score)
+            resultData[node_id] = {human_string : str(score)}
 
         answer = labels[top_k[0]]
         return resultData

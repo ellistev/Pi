7c020a164857249879e15a5479f50d91d5dc6ce2
Steven Elliott <steven.elliott@trader.ca>
2017-03-20 09:01:53 -0700

    adding new model for decency checks.

--- Autotrader/label_image/get_images_fast.py
+++ Autotrader/label_image/get_images_fast.py
@@ -1,17 +1,17 @@
 import concurrent.futures
 import urllib.request
 
-carname = 'heavy_equipment'
+carname = 'art'
 filename = ''
-lines = tuple(open('D:/Code/Pi/Autotrader/label_image/npv/heavy_equipment.txt', 'r'))
+lines = tuple(open('D:/Code/Pi/Autotrader/person/art.txt', 'r'))
 photohelperurl = 'http://az413908.vo.msecnd.net'
 underscore = '_'
 
 def getimg(count):
-    localpath = 'D:/Code/prepared_images_npv/{0}/{0}{1}.jpg'.format(carname, underscore+str(count))
-    urllib.request.urlretrieve(photohelperurl + "/" + lines[count], localpath)
+    localpath = 'D:/Code/prepared_images_bad/racy/{0}{1}.jpg'.format(carname, underscore+str(count))
+    urllib.request.urlretrieve(lines[count], localpath)
     #URLS[count] = localpath
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=50) as e:
-    for i in range(1000):
+    for i in range(600):
         e.submit(getimg, i)
